4737fceefe51b71a89b697da0ff29f4f48f5cc69
gcoronado28 <gabrielomar2809@gmail.com>
2019-01-27 22:36:42 +0000

    Backing Up - API already started

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,22 @@
 (function() {
   'use strict';
   
-  angular.module('movies-app', [])}
-)();
+  angular
+    .module('movies-app', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider){
+      
+      $routeProvider
+        .when('/', {
+        templateUrl: 'views/movies.html'
+      }).when('/movies', {
+        templateUrl: 'views/movies.html'
+      }).when('/series', {
+        templateUrl: 'views/series.html'
+      }).when('/favs', {
+        templateUrl: 'views/favs.html'
+      }).otherwise({
+        redirectTo: '/'
+      });
+    
+  }])
+})();
--- assets/js/app_controller.js
+++ assets/js/app_controller.js
@@ -5,55 +5,74 @@
   .module('movies-app')
   .controller('app-controller', ctrl1);
 
-  function ctrl1(){
-    /* jshint validthis: true */
+  function ctrl1($http){
+    /*jshint validthis: true*/
     var vm = this;
-
-    vm.calc = calc;
+    vm.moviesToLoad = false;
     vm.years = [2010, 2011, 2012, 2013, 2014, 2015];
-    vm.genres = ['Acción', 'Aventura', 'Animación', 'Comedia', 'Documentales'];
-    vm.movies = [
-      {
-        imgUrl: "https://s3.amazonaws.com/palapadata/peliculas/lego-ninjago-la-pelicula.jpg",
-        title: "DC's Legends of tomorrow.",
-        rating: 7.5,
-        duration: "2h 16 min",
-        date: "5 May 2017 (USA)",
-        genre: "Acción y Aventura",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, quibusdam dolore iste quaerat est rerum nihil sit cupiditate fugit tenetur sequi unde, facilis obcaecati necessitatibus corporis accusamus a beatae quasi."
-      },
-      {
-        imgUrl: "https://s3.amazonaws.com/palapadata/peliculas/lego-ninjago-la-pelicula.jpg",
-        title: "DC's Legends of tomorrow.",
-        rating: 7.5,
-        duration: "2h 16 min",
-        date: "5 May 2017 (USA)",
-        genre: "Acción y Aventura",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, quibusdam dolore iste quaerat est rerum nihil sit cupiditate fugit tenetur sequi unde, facilis obcaecati necessitatibus corporis accusamus a beatae quasi."
-      },
-      {
-        imgUrl: "https://s3.amazonaws.com/palapadata/peliculas/lego-ninjago-la-pelicula.jpg",
-        title: "DC's Legends of tomorrow.",
-        rating: 7.5,
-        duration: "2h 16 min",
-        date: "5 May 2017 (USA)",
-        genre: "Acción y Aventura",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, quibusdam dolore iste quaerat est rerum nihil sit cupiditate fugit tenetur sequi unde, facilis obcaecati necessitatibus corporis accusamus a beatae quasi."
-      },
-      {
-        imgUrl: "https://s3.amazonaws.com/palapadata/peliculas/lego-ninjago-la-pelicula.jpg",
-        title: "DC's Legends of tomorrow.",
-        rating: 7.5,
-        duration: "2h 16 min",
-        date: "5 May 2017 (USA)",
-        genre: "Acción y Aventura",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, quibusdam dolore iste quaerat est rerum nihil sit cupiditate fugit tenetur sequi unde, facilis obcaecati necessitatibus corporis accusamus a beatae quasi."
-      },
-    ]
+    vm.movieGenres = [];
+    vm.serieGenres = [];
+    vm.movies = [];
+    
+    var URL = 'https://api.themoviedb.org/3/';
+    /*var SEARCH = 'genre/movie/list';*/
+    var API_KEY = '?api_key=a1fd368308e42e448fe27b98fc6989ef';
+    var LANG = "&language=";
+    var QUERY = "&query=";
+    vm.IMGS_PATH = 'http://image.tmdb.org/t/p/w500';
+    vm.getMoviesGenres = getMovieGenres();
+    vm.getSerieGenres = getSerieGenres();
+    vm.enterPressed = enterPressed;
+    
+    /*$scope.SITE_PATH="http://image.tmdb.org/t/p/w500/"
+    $scope.title = 'search ctrl';
+    $scope.moviesLoaded=false;*/
 
-    function calc(){
-     vm.result = vm.first + vm.second;
+    function getMovieGenres() {
+      var promise = $http.get(URL + 'genre/movie/list' + API_KEY);
+      promise.then(successCallback, failureCallback)
+      function successCallback(result) {
+        vm.movieGenres = result.data.genres
+      }
+      function failureCallback(result) {
+        console.log("Error", result)
+      }
+    }
+    
+    function getSerieGenres() {
+      var promise = $http.get(URL + 'genre/tv/list' + API_KEY);
+      promise.then(successCallback, failureCallback)
+      function successCallback(result) {
+        vm.serieGenres = result.data.genres
+      }
+      function failureCallback(result) {
+        console.log("Error", result)
+      }
+    } 
+    
+    
+    function enterPressed(keyEvent) {
+      var input = document.getElementById('input-search');
+      if (keyEvent.which === 13){
+        //console.log("Clicked")
+        getMovies('search/movie', input.value);
+        input.blur();
+      }
+    }
+    
+    function getMovies(search, query) {
+      var promise = $http.get(URL + search + API_KEY + QUERY + query + '&page=1');
+      promise.then(successCallback, failureCallback)
+      function successCallback(result) {
+        vm.movies = result.data.results;
+        console.log(result)
+      }
+      function failureCallback(result) {
+        alert("Debe ingresar una búsqueda!")
+        console.log("Error", result)
+      }
     }
 
-}
+  
+  }
 })();
