61d1bad31926542073308e3a6c06332f0d5eb830
gcoronado28 <gabrielomar2809@gmail.com>
2019-01-28 06:00:27 +0000

    More. Pagination. Methods

--- assets/js/app_controller.js
+++ assets/js/app_controller.js
@@ -14,18 +14,28 @@
     vm.serieGenres = [];
     vm.movies = [];
     vm.favs = [];
+    vm.prev = prev;
+    vm.next = next;
+    vm.deleteFavs = deleteFavs;
+    
+    vm.total_pages = 0;
+    vm.page = 1;
+
+   
     
     var URL = 'https://api.themoviedb.org/3/';
     /*var SEARCH = 'genre/movie/list';*/
     var API_KEY = '?api_key=a1fd368308e42e448fe27b98fc6989ef';
     var LANG = "&language=";
     var QUERY = "&query=";
+    var PAGE = "&page=";
     vm.IMGS_PATH = 'http://image.tmdb.org/t/p/w500';
     vm.getMoviesGenres = getMovieGenres();
     vm.getSerieGenres = getSerieGenres();
     vm.enterPressed = enterPressed;
     vm.addFav = addFav;
     vm.fetchFavs = fetchFavs();
+    vm.tempQuery = '';
     /*$scope.SITE_PATH="http://image.tmdb.org/t/p/w500/"
     $scope.title = 'search ctrl';
     $scope.moviesLoaded=false;*/
@@ -53,27 +63,37 @@
       }
     } 
     
-    
     function enterPressed(keyEvent) {
       var input = document.getElementById('input-search');
       if (keyEvent.which === 13){
+        vm.tempQuery = input.value;
         console.log("Clicked")
-        getMovies('search/movie', input.value);
+        getMovies('search/movie', vm.tempQuery, vm.page);
         input.blur();
       }
     }
     
-    function getMovies(search, query) {
-      var promise = $http.get(URL + search + API_KEY + QUERY + query + '&page=1');
+    function getMovies(search, query, page) {
+      var promise = $http.get(URL + search + API_KEY + QUERY + query + PAGE + page);
       promise.then(successCallback, failureCallback)
       function successCallback(result) {
         vm.movies = result.data.results;
         console.log(result)
+        vm.total_pages = result.data.total_pages;
       }
       function failureCallback(result) {
-        alert("Debe ingresar una búsqueda!")
+        alert("Debe ingresar una búsqueda correcta!")
         console.log("Error", result)
       }
+    }
+    
+   function next() {
+     if (vm.page != vm.total_pages)
+       getMovies('search/movie', vm.tempQuery, ++vm.page);
+    }
+    function prev() {
+      if (vm.page - 1 > 0)
+        getMovies('search/movie', vm.tempQuery, --vm.page);
     }
     
     function addFav(event){
@@ -99,19 +119,19 @@
     }
     
     function fetchFavs(){
-      //localStorageService.clearAll();
       vm.favs = [];
-      //localStorageService.remove('key');
-     
       var array = localStorageService.keys();
       for (var i = 0; i < array.length; i++) {
         vm.favs.push(localStorageService.get(array[i]));
       }
-      console.log(vm.favs);
     }
+    
+    function deleteFavs(){
+      localStorageService.clearAll();
+    }
+    
   
-  
- }
+ } //End Controller Function
     
     
 })();
