8ea07d0232a51135bde2d0831cac6a09e236533d
gcoronado28 <gabrielomar2809@gmail.com>
2019-01-29 19:27:44 +0000

    Series new

--- assets/js/app_controller.js
+++ assets/js/app_controller.js
@@ -22,6 +22,8 @@
     vm.page = 1;
     vm.trailerUrl = '';
     vm.genre = '';
+    vm.genre_tv = '';
+    vm.searchValue = '';
     vm.year = 0;
     
     vm.getMoviesGenres = getMovieGenres();
@@ -31,6 +33,8 @@
     vm.addFav = addFav;
     vm.findByGenre = findByGenre;
     vm.findByYear = findByYear;
+    vm.findByYearTv = findByYearTv;
+    vm.findByGenreTv = findByGenreTv;
     vm.getTrailer = getTrailer;
     vm.fetchFavs = fetchFavs();
     
@@ -48,7 +52,7 @@
         console.log("Error", result)
       }
     }
-    
+
     function getSerieGenres() {
       var promise = $http.get(movService.URL + 'genre/tv/list' + movService.API_KEY);
       promise.then(successCallback, failureCallback)
@@ -60,45 +64,37 @@
       }
     } 
     
-    function enterPressed(keyEvent) {
-      var input = document.getElementById('input-mov');
-      if (keyEvent.which === 13){
-        movService.globalSearch = 'search/movie'
-        vm.tempQuery = input.value;
-        getMovies(vm.tempQuery, vm.page_tv);
-        input.blur();
-      }
-    }
-    
-    function enterPressedTv(keyEvent) {
-      var input = document.getElementById('input-tv');
-      if (keyEvent.which === 13){
-        movService.globalSearch = 'search/tv'
-        vm.tempQuery = input.value;
-        getMovies(vm.tempQuery, vm.page);
-        input.blur();
-      }
-    }
-
     function getMovies(query, page, genre='', year='') {
-      var promise = $http.get(movService.URL + movService.globalSearch + movService.API_KEY + movService.QUERY + query + movService.PAGE + page + '&include_video=true' + '&with_genres=' + genre + '&primary_release_year=' + year);
+      var promise = $http.get(movService.URL + movService.globalSearch + movService.API_KEY + movService.QUERY + query + movService.PAGE + page + '&include_video=true' + '&with_genres=' + genre + '&%e_year=' + year);
       promise.then(successCallback, failureCallback)
       function successCallback(result) {
         if(movService.globalSearch.endsWith('movie')) {
           vm.movies = result.data.results;
           vm.total_pages = result.data.total_pages;
-          console.log(vm.movies);
         }
         if(movService.globalSearch.endsWith('tv')){
           vm.series = result.data.results;
           vm.total_pages_tv = result.data.total_pages;
-          console.log(vm.series);
         } 
       }
       function failureCallback(result) {
         alert("Debe ingresar una búsqueda correcta!")
         console.log("Error", result)
       }
+    }
+    
+    function enterPressed() {
+      movService.globalSearch = 'search/movie';
+      if (vm.searchValue === '') movService.globalSearch = 'discover/movie';
+      vm.tempQuery = vm.searchValue;
+      getMovies(vm.tempQuery, vm.page);
+    }
+    
+    function enterPressedTv(keyEvent) {
+      movService.globalSearch = 'search/tv';
+      if (vm.searchValue === '') movService.globalSearch = 'discover/tv';
+      vm.tempQuery = vm.searchValue;
+      getMovies(vm.tempQuery, vm.page_tv);
     }
     
    function next(val) {
@@ -110,7 +106,7 @@
          break;
       case 1:
          if (vm.page_tv != vm.total_pages){
-           getMovies(vm.tempQuery, ++vm.page_tv, vm.genre.id);
+           getMovies(vm.tempQuery, ++vm.page_tv, vm.genre_tv.id);
          }
          break;
       }
@@ -125,7 +121,7 @@
          break;
       case 1:
          if (vm.page_tv - 1 > 0){
-           getMovies(vm.tempQuery, --vm.page_tv, vm.genre.id);
+           getMovies(vm.tempQuery, --vm.page_tv, vm.genre_tv.id);
          }
          break;
       }
@@ -169,18 +165,31 @@
     function findByGenre() {
       vm.tempQuery = '';
       movService.globalSearch = 'discover/movie';
-      console.log(vm.genre.id);
       getMovies(vm.tempQuery, vm.page, vm.genre.id, vm.year);
     }
     
     function findByYear() {
       vm.tempQuery = '';
       movService.globalSearch = 'discover/movie';
-      console.log(vm.year);
       getMovies(vm.tempQuery, vm.page, vm.genre.id, vm.year);
     }
     
+    function findByGenreTv() {
+      vm.tempQuery = '';
+      movService.globalSearch = 'discover/tv';
+      getMovies(vm.tempQuery, vm.page_tv, vm.genre_tv.id, vm.genre_tv);
+    }
+    
+    function findByYearTv() {
+      vm.tempQuery = '';
+      movService.globalSearch = 'discover/tv';
+      getMovies(vm.tempQuery, vm.page_tv, vm.genre_tv.id, vm.year);
+    }
+    
     getMovies('', vm.page);
+    /*movService.globalSearch = 'discover/tv';
+    getMovies('', vm.page_tv);*/
+    
     
  } //End Controller Function
     
